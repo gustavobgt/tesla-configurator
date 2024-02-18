82dbac9b12ff9a927ee33c31978aa0be7c06aab9
gustavobgt <gustavo_bgt97@hotmail.com>
2024-02-18 17:35:15 -0300

    feat: add steps feature routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'step-one', pathMatch: 'full' },
+  {
+    path: 'step-one',
+    loadComponent: () =>
+      import('./step-one/step-one.component').then(
+        (mod) => mod.StepOneComponent
+      ),
+  },
+  {
+    path: 'step-two',
+    loadComponent: () =>
+      import('./step-two/step-two.component').then(
+        (mod) => mod.StepTwoComponent
+      ),
+  },
+  {
+    path: 'step-three',
+    loadComponent: () =>
+      import('./step-three/step-three.component').then(
+        (mod) => mod.StepThreeComponent
+      ),
+  },
+];
