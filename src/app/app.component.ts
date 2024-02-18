85a4280d5e95f312e5b737d97a309145696fd0ab
gustavobgt <gustavo_bgt97@hotmail.com>
2024-02-18 17:44:34 -0300

    feat: add tabs to navigate between steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {AsyncPipe, JsonPipe} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, RouterOutlet],
+  imports: [AsyncPipe, JsonPipe, RouterOutlet, TabsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
