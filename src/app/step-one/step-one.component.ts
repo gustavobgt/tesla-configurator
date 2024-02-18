2484d45f83ff998a2104ba9e4c69911db4c8e827
gustavobgt <gustavo_bgt97@hotmail.com>
2024-02-18 18:19:39 -0300

    feat: add model selector and populate options from api

--- src/app/step-one/step-one.component.ts
+++ src/app/step-one/step-one.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CarModelService } from './services/car-model.service';
+import { Observable } from 'rxjs';
+import { CarModel } from '../models/car-model.model';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-step-one',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, NgFor, AsyncPipe, NgIf],
+  providers: [CarModelService],
   templateUrl: './step-one.component.html',
-  styleUrl: './step-one.component.scss'
+  styleUrl: './step-one.component.scss',
 })
-export class StepOneComponent {
+export class StepOneComponent implements OnInit {
+  carModels$!: Observable<CarModel[]>;
 
+  constructor(private carModelService: CarModelService) {}
+
+  ngOnInit(): void {
+    this.carModels$ = this.getCarModels();
+  }
+
+  getCarModels() {
+    return this.carModelService.getCarModels();
+  }
 }
